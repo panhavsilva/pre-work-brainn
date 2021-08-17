052a19be97e974c0ca587c9ae90e5c41094bd044
pamela vieira da silva <panhavsilva@gmail.com>
2021-08-17 13:46:10 -0300

    chalenge 04

--- dia1/src/main.js
+++ dia1/src/main.js
@@ -1,6 +1,28 @@
 import './style.css'
 
-document.querySelector('[data-js=app]').innerHTML = `
+const app = document.querySelector('[data-js="app"]')
+app.innerHTML = `
   <h1>B. Academy</h1>
   <p>Boas vindas à semana de pré-work para o Bootcamp em React.js 😁</p>
 `
+
+const linkMesage = {
+  initial: 'Clique aqui para ocultar o conteúdo!',
+  close: (link, app) => {
+    app.style.opacity = 0;
+    return link.innerHTML = 'Clique aqui para <b>mostrar</b> o conteúdo!'
+  },
+  open: (link, app) => {
+    app.style.opacity = 1;
+    return link.innerHTML = 'Clique aqui para <b>ocultar</b> o conteúdo!'
+  },
+}
+
+const link = document.querySelector('[data-js="link"]')
+link.addEventListener('click', (event) => {
+  event.preventDefault()
+
+  return link.innerText === linkMesage.initial
+    ? linkMesage.close(link, app)
+    : linkMesage.open(link, app)
+}, false)
