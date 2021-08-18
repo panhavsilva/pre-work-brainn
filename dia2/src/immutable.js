e8c8e3a80290614f34f35c88778d3566cba54d3b
pamela vieira da silva <panhavsilva@gmail.com>
2021-08-18 09:02:22 -0300

    chalenge 01

---
+++ dia2/src/immutable.js
@@ -0,0 +1,15 @@
+const john = {
+  name: 'John',
+  surname: 'Doe',
+  age: 30,
+  hobbies: ['Surf', 'Design'],
+}
+
+const jane = {
+  ...john,
+  name: 'Jane',
+  hobbies: john.hobbies.concat('MuayThai', 'Programming'),
+}
+
+console.log('John:', john)
+console.log('Jane:', jane)
